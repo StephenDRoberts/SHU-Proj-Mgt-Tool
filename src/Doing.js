6961fc6c3caa74dc37fe4efe05324b4b11c3ab8e
Stephen Roberts <sro01@sbgml01231.sbg.skybettingandgaming.com>
2018-09-23 14:59:28 +0100

    proj mgt tool - intial commit

---
+++ src/Doing.js
@@ -0,0 +1,104 @@
+import React, {Component} from 'react';
+import Ticket from './Ticket.js'; 
+
+let tasksAr = [
+    {
+        "user": "user1",
+        "title": "Washing Up",
+        "description": "To do the washing up",
+        "estHours": 1,
+        "actHours": "",
+        "status": "To-Do",
+        "type":"Chores",
+        "priority": 1
+    },
+    {
+        "user": "user1",
+        "title": "Code",
+        "description": "Learn Kotlin",
+        "estHours": 5,
+        "actHours": "",
+        "status": "Doing",
+        "type":"Pers Dev",
+        "priority": 1
+    },
+    {
+        "user": "user1",
+        "title": "Iron",
+        "description": "Iron my clothes",
+        "estHours": 1,
+        "actHours": "",
+        "status": "To-Do",
+        "type":"Chores",
+        "priority": 2
+    },
+    {
+        "user": "user1",
+        "title": "Play guitar",
+        "description": "Don't fear the reaper",
+        "estHours": 1,
+        "actHours": 1,
+        "status": "Done",
+        "type":"Fun",
+        "priority": 1
+    },
+    {
+        "user": "user1",
+        "title": "Watch Everton",
+        "description": "Arsenal vs Everton",
+        "estHours": 3,
+        "actHours": "",
+        "status": "To-Do",
+        "type":"Fun",
+        "priority": 3
+    },
+    {
+        "user": "user1",
+        "title": "Hoover",
+        "description": "Hoover the house",
+        "estHours": 1,
+        "actHours": 1,
+        "status": "Done",
+        "type":"Chores",
+        "priority": 2
+    },
+    {
+        "user": "user1",
+        "title": "Have lunch",
+        "description": "",
+        "estHours": 1,
+        "actHours": "",
+        "status": "Doing",
+        "type":"To live",
+        "priority": 2
+    },
+]
+
+
+let doingAr = [];
+
+for(var i=0; i<tasksAr.length; i++){
+    if(tasksAr[i].status=="Doing"){
+        doingAr.push(tasksAr[i])
+    }
+}
+
+
+let doingTicketsAr = [];
+doingTicketsAr = doingAr.map(function (obj, i) {
+    return <Ticket key={i} data = {obj}></Ticket>
+  })
+
+class Doing extends React.Component {
+render(){
+    return (
+    <div>
+        <h1>Doing:</h1>
+        {doingTicketsAr}
+    </div>
+    )
+}
+
+
+}
+export default Doing
---
+++ src/Ticket.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+
+
+class Ticket extends React.Component {
+
+
+    render() {
+        return (
+            <div className='tickets'>
+                <h4 className='ticketTitle'>{this.props.data.title}</h4>
+                <p className='estHours'>Est hours: {this.props.data.estHours} hrs</p>
+                <p className='taskType'>{this.props.data.type}</p>
+            </div>
+        )
+    }
+
+
+}
+export default Ticket
